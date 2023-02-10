d0ed8ab1614294413b23ea89805e20abced59759
andi1072 <123432653+andi1072@users.noreply.github.com>
2023-02-10 10:00:10 +0700

    Update

--- api/database/migrations/2023_01_17_141350_create_x_obu_devices_table.php
+++ api/database/migrations/2023_01_17_141350_create_x_obu_devices_table.php
@@ -20,12 +20,18 @@ return new class extends Migration
             $table->string('ftdescriptio',255)->nullable();
             $table->float('fflat')->default(0);
             $table->float('fflon')->default(0);
+            $table->integer('fngeo_status')->default(0);
+            $table->integer('fngeo_current_id')->default(0);
+            $table->integer('fngeo_chkpoint')->default(0);
             $table->timestamp('created_at');
             $table->timestamp('updated_at');
             $table->index([
                 'ftimei',
                 'ftname',
                 'fttype',
+                'fngeo_status',
+                'fngeo_current_id',
+                'fngeo_chkpoint',
                 'created_at',
                 'updated_at'
             ]);
